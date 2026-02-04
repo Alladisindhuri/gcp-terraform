c061326cdbc87172de0540c57c7d39446d63c870
sindhuri <alladisindhuri@gmail.com>
2026-02-05 00:13:02 +0530

    Add project in subnets.tf

--- flip_app/subnets.tf
+++ flip_app/subnets.tf
@@ -3,6 +3,7 @@ resource "google_compute_subnetwork" "flip-app-subnet" {
   ip_cidr_range = "192.168.2.0/24"
   region        = var.region
   network       = google_compute_network.flip_vpc.id
+  project= local.flip_network_Project
 
   private_ip_google_access = true
 }
