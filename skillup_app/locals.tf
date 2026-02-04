d41180604a3a8a4d401721934ee292991c9ed693
sindhuri <alladisindhuri@gmail.com>
2026-02-05 00:19:51 +0530

    Create locala.tf in skillup_file

---
+++ skillup_app/locals.tf
@@ -0,0 +1,5 @@
+locals{
+    skillup_network_project = google_project.skillup_network_project.project_id
+    skillup_computeandstorage_project = google_project.skillup_computeandstorage_prj.project_id
+
+}
