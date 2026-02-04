3848b0f6f851248fc2beae01a2e8e5ee4708bfca
sindhuri <alladisindhuri@gmail.com>
2026-02-04 23:48:01 +0530

    Create vpc file in Flip app

--- flip_app/vpc.tf
+++ flip_app/vpc.tf
@@ -1,6 +1,7 @@
 resource "google_compute_network" "flip_vpc" {
   name                    = "flip-vpc"
   auto_create_subnetworks = false   # custom mode VPC
-  routing_mode            = "REGIONAL"
+  routing_mode            = "GLOBAL"
+  project = local.flip_network_Project
 }
 
