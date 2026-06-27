bd055e38d02406d9b0f048e0affdb58dfcdf848d
sindhuri <alladisindhuri@gmail.com>
2026-06-27 21:55:43 +0530

    edit the CIDR subnet mask

--- flip_app/firewalls.tf
+++ flip_app/firewalls.tf
@@ -7,6 +7,6 @@ resource "google_compute_firewall" "allow_http" {
     ports    = ["80"]
   }
 
-  source_ranges = ["10.10.10.10/0"]
+  source_ranges = ["10.10.10.10/24"]
   target_tags   = ["web-server"]
 }
